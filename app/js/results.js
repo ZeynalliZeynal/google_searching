fe3066938c039baa524ec16273ac6b88247e06ae
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-02-11 03:28:08 +0400

    html & css finished

--- app/js/results.js
+++ app/js/results.js
@@ -10,3 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchInput = Storage.getVariable().inputValue;
   document.title = searchInput + ' - Google Search';
 });
+
+const url = 'https://pixabay.com/api/?key=24090419-925e057925ba4cc124682bb5f';
+async function getData() {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
